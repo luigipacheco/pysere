785ca405a3aabff7e255af919a06fd3f8edbb784
luigipacheco <luigipach@gmail.com>
2015-05-02 15:36:43 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import fileinput
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+
+
+	
+class AccountDetailsForm(BoxLayout):
+	
+	def searchreplace(self):
+		search_box = ObjectProperty()
+		replace_box= ObjectProperty()
+		dir_box = ObjectProperty()
+		
+		print ("Text to search for:" + self.search_box.text)
+		textToSearch = self.search_box.text
+
+		print ("Text to replace it with:" + self.replace_box.text)
+		textToReplace = self.replace_box.text
+
+		print ("File to perform Search-Replace on:" + self.dir_box.text)
+		fileToSearch  = self.dir_box.text
+		#fileToSearch = 'D:\dummy1.txt'
+		
+		tempFile = open( fileToSearch, 'r+' )
+		for line in fileinput.input( fileToSearch ):
+			if textToSearch in line :
+				print('Match Found')
+			else:
+				print('Match Not Found!!')
+			tempFile.write( line.replace( textToSearch, textToReplace ) )
+		tempFile.close()
+		
+		
+		print(self.search_box.text)
+		print(self.replace_box.text)
+		print(self.dir_box.text)
+		print("You just clicked the F!@#$ button!!")
+		print("Text Changed!!")
+	
+	
+class pysere(App):
+	pass
+	
+	
+pysere().run()
